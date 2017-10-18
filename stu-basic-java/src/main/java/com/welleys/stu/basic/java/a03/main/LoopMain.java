4ee5574376b7baa0b9532d288971b5bc5e156ced
welleyS <welleyS@163.com>
2017-10-18 18:03:54 +0800

    for loop, and update ignore settings

---
+++ stu-basic-java/src/main/java/com/welleys/stu/basic/java/a03/main/LoopMain.java
@@ -0,0 +1,10 @@
+package com.welleys.stu.basic.java.a03.main;
+
+public class LoopMain {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1; i++) {
+            System.out.println(i);// not output
+        }
+    }
+}
+
