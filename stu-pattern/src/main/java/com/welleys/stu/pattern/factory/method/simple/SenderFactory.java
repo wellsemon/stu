416825687dd4d1c79a84ea0bc49d5f8d77dcd5c7
suzw1 <suzw1@lenovo.com>
2018-07-11 14:34:42 +0800

    简单工厂模式-测试类移到test根目录；工厂类和方法置为public

--- stu-pattern/src/main/java/com/welleys/stu/pattern/factory/method/simple/SenderFactory.java
+++ stu-pattern/src/main/java/com/welleys/stu/pattern/factory/method/simple/SenderFactory.java
@@ -5,16 +5,16 @@ package com.welleys.stu.pattern.factory.method.simple;
  * @date 2018/07/11
  */
 class SenderFactory {
-    static final String MAIL = "mail";
-    static final String SMS = "sms";
+  static final String MAIL = "mail";
+  static final String SMS = "sms";
 
-    static Sender newInstance(String targetSent) {
-        if (MAIL.equals(targetSent)) {
-            return new MailSender();
-        } else if (SMS.equals(targetSent)) {
-            return new SmsSender();
-        }
-
-        return null;
+  static Sender newInstance(String targetSent) {
+    if (MAIL.equals(targetSent)) {
+      return new MailSender();
+    } else if (SMS.equals(targetSent)) {
+      return new SmsSender();
     }
+
+    return null;
+  }
 }
